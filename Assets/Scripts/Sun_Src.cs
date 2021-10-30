de5bf6b73df96a6946dee7cc6b5ae554afcb8200
David Marshall <dmarsh12@student.fitchburgstate.edu>
2021-10-30 18:41:53 -0400

    Water Shimmer Background

---
+++ Assets/Scripts/Sun_Src.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun_Src : MonoBehaviour
+{
+
+    //set Sunlight "Alpha Blend on Overlap" to TRUE for right before dusk.
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
